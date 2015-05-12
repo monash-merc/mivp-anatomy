78320d70e0408572ef6831b2decbc41f426a24a8
Jonathan Khoo <jonathan@whatsbeef.net>
2015-05-12 17:35:13 +1000

    changed to agnostic location; use env variable SLICERRC instead of copying file to ~/.slicerrc.py

--- launch_slicer_on_massive.sh
+++ launch_slicer_on_massive.sh
@@ -3,10 +3,20 @@
 # Copy current version of sliccerc.py to ~/.sliccer.py
 
 # MASSIVE - project monash063
-SLICER_PROJECT_FOLDER=/home/jonathankhoo/Monash063/mivp-anatomy/
+SLICER_PROJECT_FOLDER=/home/projects/Monash063/mivp-anatomy/
 SLICERRC_FILENAME=slicerrc.py
 
-cp -v -u ${SLICER_PROJECT_FOLDER}/${SLICERRC_FILENAME} ~/.slicerrc.py
+# cp -v -u ${SLICER_PROJECT_FOLDER}/${SLICERRC_FILENAME} ~/.slicerrc.py
+
+slicerrc_file=${SLICER_PROJECT_FOLDER}/${SLICERRC_FILENAME}
+
+if [ ! -e "$slicerrc_file" ]; then
+  echo "Could not find slicerrc.py file at $slicerrc_file. Exiting"
+  exit
+fi 
+
+# This must point to the slicerrc.py file
+export SLICERRC=${slicerrc_file}
 
 module load 3dslicer
 module load virtualgl
