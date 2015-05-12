3cf64f2e538d9f48c645e548a6ff57f2071fd2a3
jonathan khoo <jonathankhoo@m2-login3.massive.org.au>
2015-05-12 11:31:43 +1000

    only copy if update is required

--- launch_slicer_on_massive.sh
+++ launch_slicer_on_massive.sh
@@ -6,7 +6,7 @@
 SLICER_PROJECT_FOLDER=/home/jonathankhoo/Monash063/mivp-anatomy/
 SLICERRC_FILENAME=slicerrc.py
 
-cp -v ${SLICER_PROJECT_FOLDER}/${SLICERRC_FILENAME} ~/.slicerrc.py
+cp -v -u ${SLICER_PROJECT_FOLDER}/${SLICERRC_FILENAME} ~/.slicerrc.py
 
 module load 3dslicer
 module load virtualgl
